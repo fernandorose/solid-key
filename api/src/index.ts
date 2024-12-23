d40a4201d49f328f607a5c6d945504590b78221c
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2024-12-23 17:34:37 -0600

    Add CORS and cookie-parser middleware to the Express server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import http from "http";
 import { Server } from "socket.io";
 import pool from "./database/connection";
@@ -24,6 +26,9 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use();
 app.use("/api", userRoutes);
 app.use("/api", passwordRoutes);
 

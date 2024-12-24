b58357227ec1ae6e07edaecaea3ed93afbfb6812
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2024-12-24 10:59:06 -0600

    Add navigation button to Auth page in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,19 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import Layout from "../layouts/Layout";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <Layout title="Home">
       <main>
         <h1>Home</h1>
+        <button
+          onClick={() => {
+            navigate("/auth");
+          }}
+        >
+          Auth
+        </button>
       </main>
     </Layout>
   );

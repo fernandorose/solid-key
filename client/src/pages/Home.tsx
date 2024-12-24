63a1aba8c6769d174f3efcf0ece0450a95df91bd
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2024-12-23 21:10:01 -0600

    Update index.html title and add Home page component with layout

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Layout from "../layouts/Layout";
+
+const Home = () => {
+  return (
+    <Layout title="Home">
+      <main>
+        <h1>Home</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default Home;

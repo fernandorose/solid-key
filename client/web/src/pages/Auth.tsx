f8e327fb096d9846c6c49d43f38f5c50e9cc0cf1
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-01-03 20:43:09 -0600

    Migrate client code to new structure and remove deprecated files

---
+++ client/web/src/pages/Auth.tsx
@@ -0,0 +1,13 @@
+import Layout from "../layouts/Layout";
+
+const Auth = () => {
+  return (
+    <Layout title="Auth">
+      <main>
+        <h1>Auth</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default Auth;
--- web/src/components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-const Logo = () => {
-  return <img src="./logo.svg" alt="" />;
-};
-
-export default Logo;

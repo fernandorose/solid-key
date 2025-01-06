34193b57026dcbf4a03e36635714a1553d4519a9
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-01-06 03:35:23 -0600

    Enhance input and button styles with transitions and focus effects; add Logout button component

--- client/web/src/components/styledComponents.style.ts
+++ client/web/src/components/styledComponents.style.ts
@@ -6,6 +6,11 @@ export const Input = styled.input`
   border: solid 1px #00000011;
   padding: 10px;
   border-radius: 5px;
+  transition: 300ms;
+  &:focus {
+    border: solid 1px var(--accent-color);
+    box-shadow: 0 5px 10px rgb(241, 241, 241);
+  }
   &::placeholder {
     color: #00000044;
   }
@@ -14,8 +19,21 @@ export const Input = styled.input`
 export const Button = styled.button`
   padding: 10px;
   color: #fff;
-  background: radial-gradient(67.36% 100% at 50% 0%, #4a4a4a 0%, #000000 100%);
+  background: #000;
   border: 1px solid #000000;
-  box-shadow: inset 0px 2px 0px rgba(255, 255, 255, 0.27);
+  //box-shadow: inset 0px 2px 0px rgba(255, 255, 255, 0.46);
   border-radius: 5px;
+  transition: 300ms;
+`;
+
+export const Logout = styled.button`
+  width: 100%;
+  padding: 5px;
+  color: #fff;
+  background: var(--alert-color);
+  border: 1px solid var(--alert-color);
+  border-radius: 5px;
+  transition: 300ms;
+  font-family: var(--mono-font-2);
+  font-size: 0.85rem;
 `;

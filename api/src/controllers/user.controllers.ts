1a4d272c29719394b3bcb1ae7c5fb8779f929e38
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-01-06 03:33:52 -0600

    Add last_login field to user data and implement getUserById endpoint

--- api/src/controllers/user.controllers.ts
+++ api/src/controllers/user.controllers.ts
@@ -25,6 +25,7 @@ export const getUsers = async (req: Request, res: Response) => {
         u.name AS user_name,
         u.email AS user_email,
         u.role AS user_role,
+        u.last_login AS last_login,
         pc.id AS category_id,
         pc.category_name AS category_name,
         pc.created_at AS category_created_at,
@@ -52,6 +53,7 @@ export const getUsers = async (req: Request, res: Response) => {
       name: string;
       email: string;
       role: string;
+      last_login: string;
       categories: {
         id: string;
         name: string;
@@ -79,6 +81,7 @@ export const getUsers = async (req: Request, res: Response) => {
           name: row.user_name,
           email: row.user_email,
           role: row.user_role,
+          last_login: row.last_login,
           categories: [],
         });
       }
@@ -121,6 +124,109 @@ export const getUsers = async (req: Request, res: Response) => {
 
     // Devolver los usuarios separados en dos arreglos
     res.status(200).json({ admins: adminUsers, users: regularUsers });
+  } catch (err) {
+    res.status(500).json({ message: (err as Error).message });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const userId = req.params.id; // Obtén el user_id de los parámetros de la solicitud
+
+  try {
+    const userData = await pool.query(
+      `
+      SELECT 
+        u.id AS user_id,
+        u.name AS user_name,
+        u.email AS user_email,
+        u.role AS user_role,
+        u.last_login AS last_login,
+        pc.id AS category_id,
+        pc.category_name AS category_name,
+        pc.created_at AS category_created_at,
+        p.id AS password_id,
+        p.site_name AS site_name,
+        p.site_url AS site_url,
+        p.site_username AS site_username,
+        p.site_password AS site_password,
+        p.created_at AS password_created_at
+      FROM 
+        users u
+      LEFT JOIN 
+        password_categories pc ON u.id = pc.user_id
+      LEFT JOIN 
+        password_category_mapping pcm ON pc.id = pcm.category_id
+      LEFT JOIN 
+        passwords p ON pcm.password_id = p.id
+      WHERE
+        u.id = $1 -- Filtra por el id del usuario
+      ORDER BY 
+        pc.id, p.id;
+    `,
+      [userId]
+    );
+
+    if (userData.rows.length === 0) {
+      res.status(404).json({ message: "Usuario no encontrado" });
+      return;
+    }
+
+    const user: {
+      id: string;
+      name: string;
+      email: string;
+      role: string;
+      last_login: string;
+      categories: {
+        id: string;
+        name: string;
+        createdAt: string;
+        passwords: {
+          id: string;
+          siteName: string;
+          siteUrl: string;
+          siteUsername: string;
+          sitePassword: string;
+          createdAt: string;
+        }[];
+      }[];
+    } = {
+      id: userData.rows[0].user_id,
+      name: userData.rows[0].user_name,
+      email: userData.rows[0].user_email,
+      role: userData.rows[0].user_role,
+      last_login: userData.rows[0].last_login,
+      categories: [],
+    };
+
+    // Mapear las categorías y contraseñas del usuario
+    for (const row of userData.rows) {
+      let category = user.categories.find((cat) => cat.id === row.category_id);
+
+      if (!category && row.category_id) {
+        category = {
+          id: row.category_id,
+          name: row.category_name,
+          createdAt: row.category_created_at,
+          passwords: [],
+        };
+        user.categories.push(category);
+      }
+
+      if (category && row.password_id) {
+        category.passwords.push({
+          id: row.password_id,
+          siteName: row.site_name,
+          siteUrl: row.site_url,
+          siteUsername: row.site_username,
+          sitePassword: row.site_password,
+          createdAt: row.password_created_at,
+        });
+      }
+    }
+
+    // Devolver el usuario con sus categorías y contraseñas
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ message: (err as Error).message });
   }
@@ -209,6 +315,13 @@ export const loginUser = async (req: Request, res: Response) => {
     const expiresIn = user.role === "admin" ? "3h" : "1h";
 
     const token = jwt.sign(payload, secret as string, { expiresIn });
+
+    // Actualizar el campo last_login con la fecha y hora actuales
+    const now = new Date();
+    await pool.query("UPDATE users SET last_login = $1 WHERE id = $2", [
+      now,
+      user.id,
+    ]);
 
     res.cookie("token", token, {
       httpOnly: true,
@@ -319,15 +432,15 @@ export const changeUserEmail = async (req: Request, res: Response) => {
   }
 };
 
-export const getUserById = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    res.status(200).json({ user: user.rows[0] });
-  } catch (err) {
-    res.status(500).json({ message: (err as Error).message });
-  }
-};
+// export const getUserById = async (req: Request, res: Response) => {
+//   const id = req.params.id;
+//   try {
+//     const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+//     res.status(200).json({ user: user.rows[0] });
+//   } catch (err) {
+//     res.status(500).json({ message: (err as Error).message });
+//   }
+// };
 
 export const deleteUserById = async (req: Request, res: Response) => {
   const id = req.params.id;

1a44701543e3ccd0a4c39f1013aabfed63f9cabc
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-01-06 23:12:39 -0600

    Add Roboto Condensed font and remove Bricolage Grotesque font from main.tsx

--- client/web/src/main.tsx
+++ client/web/src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 
 // Supports weights 100-900
 import "@fontsource-variable/inter";
+// Supports weights 100-900
+import "@fontsource-variable/roboto-condensed";
 // Supports weights 100-800
 import "@fontsource-variable/jetbrains-mono";
-// Supports weights 200-800
-import "@fontsource-variable/bricolage-grotesque";
 
 import "./styles/globals.css";
 import App from "./App.tsx";

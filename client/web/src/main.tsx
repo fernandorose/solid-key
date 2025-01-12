b66d7fb08158bc95ba188e37c6a7af4628a41b33
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-01-11 20:09:08 -0600

    Update font imports to use Fira Sans and streamline main.tsx structure

--- client/web/src/main.tsx
+++ client/web/src/main.tsx
@@ -1,17 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-// Supports weights 100-900
-import "@fontsource-variable/inter";
-// Supports weights 100-900
-import "@fontsource-variable/roboto-condensed";
-// Supports weights 100-800
-import "@fontsource-variable/jetbrains-mono";
+import '@fontsource/fira-sans/400.css';
+import '@fontsource/fira-sans/700.css';
+import '@fontsource/fira-sans/800.css';
+import './styles/globals.css';
+import App from './App.tsx';
 
-import "./styles/globals.css";
-import App from "./App.tsx";
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>

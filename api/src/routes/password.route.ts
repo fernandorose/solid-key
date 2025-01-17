737120861ff186d9bf80e09e2821ea6f9cf00881
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-01-17 14:38:00 -0600

    Refactor password and user routes to use modular controller imports for improved organization

--- api/src/routes/password.route.ts
+++ api/src/routes/password.route.ts
@@ -1,20 +1,22 @@
-import express from "express";
-import * as passwordControllers from "../controllers/password.controllers";
+import express from 'express';
+
+import * as getControllers from '@/controllers/get';
+import * as postControllers from '@/controllers/post';
+import * as deleteControllers from '@/controllers/delete';
 
 const router = express.Router();
 
 router
-  .get("/passwords/get", passwordControllers.getPasswords)
-  .get("/passwords/categories/get", passwordControllers.getPasswordsCategory)
-  .post(
-    "/passwords/categories/create",
-    passwordControllers.createPasswordCategory
-  )
-  .post("/passwords/create", passwordControllers.createPassword)
-  .post("/passwords/create/key", passwordControllers.createPasswordKey)
+  .get('/passwords/get', getControllers.getPasswords)
+  .get('/passwords/categories/get', getControllers.getPasswordsCategory)
+
+  .post('/passwords/categories/create', postControllers.createPasswordCategory)
+  .post('/passwords/create', postControllers.createPassword)
+  .post('/passwords/create/key', postControllers.createPasswordKey)
+
   .delete(
-    "/passwords/delete/:id",
-    passwordControllers.deletePasswordFromCategory
+    '/passwords/delete/:id',
+    deleteControllers.deletePasswordFromCategory
   );
 
 export default router;
--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,23 +1,27 @@
-import express from "express";
-import * as userControllers from "../controllers/user.controllers";
-import { validateToken, requireAdmin } from "../middlewares/validateToken";
+import express from 'express';
+
+import * as getControllers from '@/controllers/get';
+import * as postControllers from '@/controllers/post';
+import * as putControllers from '@/controllers/put';
+import * as deleteControllers from '@/controllers/delete';
+
+import { validateToken, requireAdmin } from '@/middlewares/validateToken';
 
 const router = express.Router();
 
 router
-  .get("/users/get", validateToken, requireAdmin, userControllers.getUsers)
-  .get("/users/get/:id", validateToken, userControllers.getUserById)
-  .post("/users/create", userControllers.createUser)
-  .post("/users/login", userControllers.loginUser)
-  .post("/users/validate-token", userControllers.validateToken)
-  .post("/users/logout", userControllers.logoutUser)
-  .post("/users/decode-token", userControllers.decodeToken)
-  .put(
-    "/users/update/email/:id",
-    validateToken,
-    userControllers.changeUserEmail
-  )
-  .delete("/users/delete", validateToken, userControllers.deleteAllUsers)
-  .delete("/users/delete/:id", validateToken, userControllers.deleteUserById);
+  .get('/users/get', validateToken, requireAdmin, getControllers.getUsers)
+  .get('/users/get/:id', validateToken, getControllers.getUserById)
+
+  .post('/users/create', postControllers.createUser)
+  .post('/users/login', postControllers.loginUser)
+  .post('/users/logout', postControllers.logoutUser)
+  .post('/users/decode-token', postControllers.decodeToken)
+  .post('/users/validate-token', postControllers.validateToken)
+
+  .put('/users/update/email/:id', validateToken, putControllers.changeUserEmail)
+
+  .delete('/users/delete', validateToken, deleteControllers.deleteAllUsers)
+  .delete('/users/delete/:id', validateToken, deleteControllers.deleteUserById);
 
 export default router;

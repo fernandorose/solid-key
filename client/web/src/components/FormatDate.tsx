bb3a399498a2e882140f74c83553fed483bc5735
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-03-19 00:48:02 -0600

    react/web frontend removed

--- client/web/src/components/FormatDate.tsx
+++
@@ -1,8 +0,0 @@
-export const formatTimestamp = (timestamp: string) => {
-  const date = new Date(timestamp);
-  return new Intl.DateTimeFormat("en-EN", {
-    dateStyle: "short",
-    timeStyle: "medium",
-    hourCycle: "h23",
-  }).format(date);
-};

80d098051301405fa74fb6416028b0e028221808
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-03-19 14:50:14 -0600

    Modify some theme colors and styles

--- client/mobile/app/src/main/java/com/app/solidkey/ui/theme/Theme.kt
+++ client/mobile/app/src/main/java/com/app/solidkey/ui/theme/Theme.kt
@@ -9,35 +9,34 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
+// Define el color de fondo
+private val BackgroundColor = Color(0xFF000000)
+private val TextColor = Color(0xFFB43E3E)
+
+// Colores para el tema oscuro
 private val DarkColorScheme = darkColorScheme(
   primary = Purple80,
   secondary = PurpleGrey80,
-  tertiary = Pink80
+  tertiary = Pink80,
+  background = BackgroundColor,
 )
 
+// Colores para el tema claro
 private val LightColorScheme = lightColorScheme(
   primary = Purple40,
   secondary = PurpleGrey40,
-  tertiary = Pink40
-
-  /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+  tertiary = Pink40,
+  background = BackgroundColor // Asignar el color de fondo
 )
 
 @Composable
 fun SolidkeyTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
-  dynamicColor: Boolean = true,
+  dynamicColor: Boolean = false,
   content: @Composable () -> Unit
 ) {
   val colorScheme = when {

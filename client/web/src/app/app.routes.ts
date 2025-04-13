cab182ec366ac4dad9ae91206a4e50b2601da86b
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-04-12 23:24:40 -0600

    Add main and auth route

--- client/web/src/app/app.routes.ts
+++ client/web/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './solidkey/pages/auth/auth.component';
+import { HomeComponent } from './solidkey/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'solidkey',
+    component: HomeComponent,
+  },
+  {
+    path: 'auth',
+    title: 'Auth',
+    component: AuthComponent,
+  },
+];

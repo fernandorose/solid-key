aa499ece6d09ee3050e2dfda00dde195dbcedda8
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-04-12 22:53:05 -0600

    Remove react ui

--- client/web/src/layouts/Layout.tsx
+++
@@ -1,9 +0,0 @@
-type LayoutProps = {
-  children: React.ReactNode;
-  title: string;
-};
-
-export const Layout = ({ children, title }: LayoutProps) => {
-  document.title = 'SolidKey - ' + title;
-  return <main>{children}</main>;
-};
--- client/web/src/pages/Auth/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '../../layouts/Layout';
-
-export const Auth = () => {
-  return (
-    <Layout title="Auth">
-      <h1>Hello World</h1>
-    </Layout>
-  );
-};

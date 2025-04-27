cadb5ce256b6f7012a8ab01cc227c7910e7f5bd8
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-04-27 11:02:20 -0600

    Add global styles and font imported in main

--- solidkey/src/main.ts
+++ solidkey/src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Supports weights 100-900
+import '@fontsource-variable/roboto-condensed';
+// Supports weights 100-900
+import '@fontsource-variable/inter';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);

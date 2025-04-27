bbebf425655ad2c24bdfcdad20d1090e702827b3
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-04-27 11:03:01 -0600

    Removed a past project

--- client/web/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-// Supports weights 100-900
-import '@fontsource-variable/inter';
-
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);

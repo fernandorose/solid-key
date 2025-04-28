7e01fdf1029f1b4043f16473556da1165b71b26f
Fernando Rodríguez <ferrodriguezsegura@gmail.com>
2025-04-28 01:25:37 -0600

    Refactor import statements and logging messages for consistency

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,46 +1,46 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import http from "http";
-import { Server } from "socket.io";
-import pool from "./database/connection";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server } from 'socket.io';
+import pool from './database/connection';
 
-import userRoutes from "./routes/user.route";
-import passwordRoutes from "./routes/password.route";
-import { validateToken } from "./middlewares/validateToken";
+import userRoutes from './routes/user.route';
+import passwordRoutes from './routes/password.route';
+import { validateToken } from './middlewares/validateToken';
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 
-  socket.on("message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("message", msg);
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('message', msg);
   });
 });
 
 app.use(express.json());
 const corsOptions = {
-  origin: "http://localhost:5173", // Solo permitir esta URL
+  origin: ['http://localhost:5173', 'http://localhost:4200'], // Solo permitir esta URL
   credentials: true, // Permitir envío de cookies y headers de autenticación
 };
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use("/api/v1", userRoutes);
-app.use("/api/v1", validateToken, passwordRoutes);
+app.use('/api/v1', userRoutes);
+app.use('/api/v1', validateToken, passwordRoutes);
 
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error("Error acquiring client", err.stack);
+    return console.error('Error acquiring client', err.stack);
   }
-  console.log("Connected to PostgreSQL");
+  console.log('Connected to PostgreSQL');
   release();
 });
 

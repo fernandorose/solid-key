0cf90b611689d3b92f3ea92c8a0b89ead34bd83d
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-05-04 13:37:29 -0600

    Fix quotation style in connection.ts and docker-compose.yml for consistency

--- api/src/database/connection.ts
+++ api/src/database/connection.ts
@@ -1,5 +1,5 @@
-import { Pool } from "pg";
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "../config/config";
+import { Pool } from 'pg';
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from '../config/config';
 
 const pool = new Pool({
   user: DB_USER,
